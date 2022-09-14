ea36c9b96d4798362514ad464b34e738d280c7e4
Mohammad Javad <iman7260@gmail.com>
2022-09-14 16:11:35 +0430

    update redis-adapter

--- app/utils/initSocket.js
+++ app/utils/initSocket.js
@@ -1,5 +1,9 @@
 const socketIO = require("socket.io");
-// const redis = require('socket.io-redis');
+const { createClient } = require("redis");
+const { createAdapter } = require("@socket.io/redis-adapter");
+
+const pubClient = createClient({ url: "redis://:Zb5FuFXrbCfEjfTU3KWaMjWz@roomgapredis:6379/0" });
+const subClient = pubClient.duplicate();
 
 function initialSocket(httpServer) {
     const io = socketIO(httpServer, {
@@ -9,7 +13,7 @@ function initialSocket(httpServer) {
         maxHttpBufferSize: 1e8
     })
     // io.adapter(redis({ host: 'localhost', port: 6379 }));
-    
+    io.adapter(createAdapter(pubClient, subClient));
     return io
 }
 
